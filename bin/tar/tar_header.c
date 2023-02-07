6482fb5fffc0637c4f2328f4cc41664f7c7aef81
bcicada <b.cicada@protonmail.com>
2023-02-07 21:19:01 +0000

    Adding doxygen

--- bin/tar/tar_header.c
+++ bin/tar/tar_header.c
@@ -7,6 +7,14 @@
 #include <string.h>
 #include "tar_header.h"
 
+/**
+ * @brief Imprime les informations de l'entrée tar.
+ * 
+ * @param header Pointeur vers la structure `tar_header` qui contient les informations à imprimer.
+ * 
+ * Cette fonction imprime le nom, la taille, la date de modification et le type de l'entrée tar
+ * pointée par `header`. Si le nom est vide (null-terminated string), la fonction ne fait rien.
+ */
 void print_header(tar_header *header) {
     if (header->name[0] != '\0') {
         printf("Nom : %s\n", header->name);
